2f80c6f0860259be1bfb854e7a72c332b8fdcc38
RedHotSly <rhsgh0@gmail.com>
2012-06-11 23:32:28 -0400

    writing unit tests (in progress)

--- tests/spec/SpecHelper.js
+++ tests/spec/SpecHelper.js
@@ -1,9 +1,64 @@
-beforeEach(function() {
-  this.addMatchers({
-    toBePlaying: function(expectedSong) {
-      var player = this.actual;
-      return player.currentlyPlayingSong === expectedSong && 
-             player.isPlaying;
+beforeEach(function () {
+
+
+    function inArray(array, el) {
+        for (var i = array.length; i--; ) {
+            if (array[i] === el) {
+                return true;
+            }
+        }
+        return false;
     }
-  });
+
+    function isEqArrays(arr1, arr2) {
+        if (arr1.length !== arr2.length) {
+            return false;
+        }
+        for (var i = arr1.length; i--; ) {
+            if (!inArray(arr2, arr1[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function toCSV(items) {
+        var value = "";
+        var first = true;
+        $.each(items, function () {
+
+            if (!first) {
+                value = value + ", ";
+            }
+            first = false;
+
+            value = value + this;
+
+        });
+        return value;
+    }
+
+
+    this.addMatchers({
+        toContainSameItemsAs: function (expected) {
+
+            this.message = function () {
+
+                var actualIds = $.map(this.actual, function (item) {
+                    return $(item).attr('id') || "\'unknown id\'"
+                });
+
+                actualIds = toCSV(actualIds);
+
+                var expectedIds = $.map(expected, function (item) {
+                    return $(item).attr('id') || "\'unknown id\'"
+                });
+
+                expectedIds = toCSV(expectedIds);
+
+                return 'expected [' + actualIds + '] to contain same items as [' + expectedIds + ']';
+            }
+            return isEqArrays(this.actual, expected);
+        }
+    });
 });
--- tests/spec/simple-expand.spec.js
+++ tests/spec/simple-expand.spec.js
@@ -1,111 +1,199 @@
-﻿// jasmine-jquery is not compatible with jquery animations
-var no_animation = { hideMode: "basic" };
+﻿
+describe("simple-expand", function () {
 
-
-/*
-This test uses the parent mode but does not test that mode per se. It just tests that toggeling works.
-*/
-describe("toggling works (no matter the searchMode)", function () {
-
-
-    describe("with a single content", function () {
-
-        beforeEach(function () {
-            loadFixtures("toggeling.html");
-            var x = $("#expander1").simpleexpand(no_animation);
-        });
-
-        it("is collapsed by default", function () {
-            expect($('#content1')).toExist();
-            expect($('#content1')).not.toBeVisible();
-        });
-
-        it("content can be expanded", function () {
-            $("#expander1").click();
-            expect($('#content1')).toBeVisible();
-        });
-
-        it("content can be collapsed again", function () {
-            $("#expander1").click();
-            $("#expander1").click();
-            expect($('#content1')).not.toBeVisible();
-        });
-    });
-
-    describe("with multiple content", function () {
-
-        beforeEach(function () {
-            loadFixtures("toggeling.html");
-            $("#expander2").simpleexpand(no_animation);
-        });
-
-        it("is collapsed by default", function () {
-            expect($('#content2')).toExist();
-            expect($('#content2')).not.toBeVisible();
-            expect($('#content3')).toExist();
-            expect($('#content3')).not.toBeVisible();
-        });
-
-        it("content can be expanded", function () {
-            $("#expander2").click();
-            expect($('#content2')).toBeVisible();
-            expect($('#content3')).toBeVisible();
-        });
-
-        it("content can be collapsed again", function () {
-            $("#expander2").click();
-            $("#expander2").click();
-            expect($('#content2')).not.toBeVisible();
-            expect($('#content3')).not.toBeVisible();
-        });
-    });
-
-
-});
-
-// Requires html fixture: parent.html
-describe("searchMode: parent", function () {
-
-    var matchedtargets;
+    var base_test_settings = {};
 
     beforeEach(function () {
-        loadFixtures("parent mode.html");
-
-        var base = $.fn.simpleexpand.fn.findTargets;
-        spyOn($.fn.simpleexpand.fn, 'findTargets').andCallFake(function (a, b, c) {
-
-            matchedtargets = base(a, b, c);
-            return matchedtargets;
-        });
-        
+        base_test_settings = { 'hideMode': 'basic' };
     });
 
-    describe("when expander is sibling of content", function () {
 
-        it("content is found", function () {
-            $("#when-expander-is-sibling-of-content #expander1").simpleexpand(no_animation);
-            expect($.fn.simpleexpand.fn.findTargets).toHaveBeenCalled();
-            expect(matchedtargets).toBe('#content1');
+    /*
+    This test uses the parent mode but does not test that mode per se. It just tests that toggeling works.
+    */
+    describe("when expanding and collapsing targets (independent of the searchMode)", function () {
+
+        describe("with a single target", function () {
+
+            beforeEach(function () {
+                loadFixtures("toggeling.html");
+                var x = $("#expander1").simpleexpand(base_test_settings);
+            });
+
+            it("is collapsed by default", function () {
+                expect($('#1000')).toExist();
+                expect($('#1000')).not.toBeVisible();
+            });
+
+            it("target can be expanded", function () {
+                $("#expander1").click();
+                expect($('#1000')).toBeVisible();
+            });
+
+            it("target can be collapsed again", function () {
+                $("#expander1").click();
+                $("#expander1").click();
+                expect($('#1000')).not.toBeVisible();
+            });
+        });
+
+        describe("with multiple targets", function () {
+
+            beforeEach(function () {
+                loadFixtures("toggeling.html");
+                $("#expander2").simpleexpand(base_test_settings);
+            });
+
+            it("is collapsed by default", function () {
+                expect($('#2000')).toExist();
+                expect($('#2000')).not.toBeVisible();
+                expect($('#2001')).toExist();
+                expect($('#2001')).not.toBeVisible();
+            });
+
+            it("targets can be expanded", function () {
+                $("#expander2").click();
+                expect($('#2000')).toBeVisible();
+                expect($('#2001')).toBeVisible();
+            });
+
+            it("targets can be collapsed again", function () {
+                $("#expander2").click();
+                $("#expander2").click();
+                expect($('#2000')).not.toBeVisible();
+                expect($('#2001')).not.toBeVisible();
+            });
+        });
+
+
+    });
+
+    // Requires html fixture: parent.html
+    describe("when searchMode is 'parent'", function () {
+
+        var matchedtargets;
+
+        beforeEach(function () {
+
+            loadFixtures("parent mode.html");
+
+            var base = $.fn.simpleexpand.fn.findTargets;
+            spyOn($.fn.simpleexpand.fn, 'findTargets').andCallFake(function (a, b, c) {
+
+                matchedtargets = base(a, b, c);
+                return matchedtargets;
+            });
+
+        });
+
+        describe("when expander is sibling of target", function () {
+
+            it("target is found", function () {
+                $("#when-expander-is-sibling-of-target .expander1").simpleexpand(base_test_settings);
+                expect($.fn.simpleexpand.fn.findTargets).toHaveBeenCalled();
+                expect(matchedtargets).toContainSameItemsAs($('#1000'));
+            });
+
+        });
+
+        describe("when expander is lower in parent than targer", function () {
+            it("target is found", function () {
+                $("#when-expander-is-lower-in-parent .expander1").simpleexpand(base_test_settings);
+                expect($.fn.simpleexpand.fn.findTargets).toHaveBeenCalled();
+                expect(matchedtargets).toContainSameItemsAs($('#2000'));
+            });
+        });
+
+        describe("when target is several parents higher than target", function () {
+            it("target is found", function () {
+                $("#when-target-is-several-parents-higher-than-target .expander1").simpleexpand(base_test_settings);
+                expect($.fn.simpleexpand.fn.findTargets).toHaveBeenCalled();
+                expect(matchedtargets).toContainSameItemsAs($('#3000'));
+            });
+        });
+
+        describe("when expander has multiple targets", function () {
+            it("targets are found", function () {
+                $("#when-expander-has-multiple-targets .expander1").simpleexpand(base_test_settings);
+                expect($.fn.simpleexpand.fn.findTargets).toHaveBeenCalled();
+                expect(matchedtargets).toContainSameItemsAs($('#4000').add('#4001').add('#4002'));
+            });
+        });
+    });
+
+
+
+    // Requires html fixture: parent.html
+    describe("when searchMode is 'absolute'", function () {
+
+        var matchedtargets;
+
+        beforeEach(function () {
+
+            $.extend(base_test_settings, { 'defaultSearchMode': 'absolute' });
+
+            loadFixtures("absolute mode.html");
+
+            var base = $.fn.simpleexpand.fn.findTargets;
+            spyOn($.fn.simpleexpand.fn, 'findTargets').andCallFake(function (a, b, c) {
+
+                matchedtargets = base(a, b, c);
+                return matchedtargets;
+            });
+
+        });
+
+        describe("when single match", function () {
+
+            it("target is found", function () {
+                $("#when-expander-target-is-specified-when-single-match .expander1").simpleexpand(base_test_settings);
+                expect($.fn.simpleexpand.fn.findTargets).toHaveBeenCalled();
+                expect(matchedtargets).toContainSameItemsAs($('#2000'));
+            });
+        });
+
+        describe("when multiple matches", function () {
+            it("targets are found", function () {
+                $("#when-expander-target-is-specified-when-multiple-matches .expander1").simpleexpand(base_test_settings);
+                expect($.fn.simpleexpand.fn.findTargets).toHaveBeenCalled();
+                expect(matchedtargets).toContainSameItemsAs($('#3000').add('#3001'));
+            });
         });
 
     });
 
-    describe("when expander is lower in parent", function () {
-        it("content is found", function () {
-            $("#when-expander-is-lower-in-parent #expander1").simpleexpand(no_animation);
-            expect($.fn.simpleexpand.fn.findTargets).toHaveBeenCalled();
-            expect(matchedtargets).toBe('#content1');
+
+    // Requires html fixture: parent.html
+    describe("when searchMode is 'relative'", function () {
+
+        var matchedtargets;
+
+        beforeEach(function () {
+
+            $.extend(base_test_settings, { 'defaultSearchMode': 'relative' });
+
+            loadFixtures("relative mode.html");
+
+            var base = $.fn.simpleexpand.fn.findTargets;
+            spyOn($.fn.simpleexpand.fn, 'findTargets').andCallFake(function (a, b, c) {
+
+                matchedtargets = base(a, b, c);
+                return matchedtargets;
+            });
+
         });
+
+        describe("when todo", function () {
+
+            it("target is found", function () {
+                $("#todo .expander1").simpleexpand(base_test_settings);
+                expect($.fn.simpleexpand.fn.findTargets).toHaveBeenCalled();
+                expect(matchedtargets).toContainSameItemsAs($('#1000'));
+            });
+        });
+
+
     });
 
-    describe("when expander is several parents higher", function () {
-        it("content is found", function () {
-            $("#when-expander-is-several-parents-higher #expander1").simpleexpand(no_animation);
-            expect($.fn.simpleexpand.fn.findTargets).toHaveBeenCalled();
-            expect(matchedtargets).toBe('#content1');
-        });
-    });
 
 });
-
-
