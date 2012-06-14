aa589f340e6a5c98e78aca987c60df3cdc5a4874
RedHotSly <rhsgh0@gmail.com>
2012-06-13 23:02:09 -0400

    reformat; create minified version

--- src/simple-expand.js
+++ src/simple-expand.js
@@ -39,24 +39,32 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
             // hideMode
             // -----------
             // Specifies method to hide the content element.
+            //
             // Default: fadeToggle
             //
             // Values:
             // - fadeToggle: Use jquery.fadeToggle()
             // - basic: Use jquery.toggle()
-            // - css: relies on user provided css to show/hide. you can defines classes for "collapsed" and "expanded" classes.
+            // - css: relies on user provided css to show/hide. you can defines
+            //   classes for "collapsed" and "expanded" classes.
             //
             // If un an unknown value is specified, the plug-in reverts to "css".
             'hideMode': 'fadeToggle',
 
             // searchMode
             // -----------
-            // Specifies the defaut value for  data-expander-target-search when none is specified on the expander element
+            // Specifies the defaut value for  data-expander-target-search
+            // when none is specified on the expander element.
+            //
             // Default: parent
             //
             // Values:
-            // - parent: go up the expander's parents hierarchy searching each parent's childens lookin for a target
-            // - absolute : finds a target globally in the document (useful when matching an id)
+            // - parent: go up the expander's parents hierarchy searching 
+            //           each parent's childens looking for a target
+            //
+            // - absolute : finds a target globally in the document (useful when 
+            //              matching an id)
+            //
             // - relative : finds a target nested inside the expander
             //
             // If un an unknown value is specified, no targets will be found.
@@ -64,13 +72,17 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
             // defaultTarget
             // -----------
-            // Specifies the defaut value for data-expander-target when none is specified on the expander element
+            // Specifies the defaut value for data-expander-target when
+            // none is specified on the expander element.
+            //
             // Default: .content
             'defaultTarget': '.content',
 
-            // onerror
+            // throwOnMissingTarget
             // -----------
-            // Specifies whether the plug-in throws an exception if it cannot find a target for the expander 
+            // Specifies whether the plug-in throws an exception if it
+            // cannot find a target for the expander 
+            //
             // Default: true
             'throwOnMissingTarget': true
         };
